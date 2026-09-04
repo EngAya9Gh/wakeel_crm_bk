b9c1dc9d2fbca3f3b9362b9b5f1cb57ebd961734
EngAya <aya.ghoury@gmail.com>
2026-09-04 17:02:32 +0300

    chore: update favicon to icon_dark.png in app.blade.php

--- resources/views/app.blade.php
+++ resources/views/app.blade.php
@@ -5,7 +5,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <title>Wakeel CRM - Super Admin</title>
 
-    <link rel="icon" href="{{ asset('icon_light.png') }}" type="image/png">
+    <link rel="icon" href="{{ asset('icon_dark.png') }}" type="image/png">
 
     <link rel="preconnect" href="https://fonts.googleapis.com">
     <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
