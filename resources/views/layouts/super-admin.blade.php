e5deed3123db10f5d090f6f9ea1530184fe621aa
EngAya <aya.ghoury@gmail.com>
2026-09-05 15:59:56 +0300

    fix: update Vite JS entry point from app.js to app.jsx in blade files

--- resources/views/layouts/super-admin.blade.php
+++ resources/views/layouts/super-admin.blade.php
@@ -9,7 +9,7 @@
     <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
     <link href="https://fonts.googleapis.com/css2?family=Tajawal:wght@300;400;500;700;800&display=swap" rel="stylesheet">
 
-    @vite(['resources/css/app.css', 'resources/js/app.js'])
+    @vite(['resources/css/app.css', 'resources/js/app.jsx'])
 
     <style>
         body {
