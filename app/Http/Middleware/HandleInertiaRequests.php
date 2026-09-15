285dfa03a651de17355ab53bb93c9063e34d4b00
EngAya <aya.ghoury@gmail.com>
2026-09-15 20:12:48 +0300

    fix: inertia returning json on direct browser visits by adding vary header

--- app/Http/Middleware/HandleInertiaRequests.php
+++ app/Http/Middleware/HandleInertiaRequests.php
@@ -8,6 +8,21 @@ use Inertia\Middleware;
 class HandleInertiaRequests extends Middleware
 {
     /**
+     * Handle the incoming request.
+     */
+    public function handle(Request $request, \Closure $next)
+    {
+        $response = parent::handle($request, $next);
+        
+        // Fix for Inertia returning JSON on direct browser visits (Vary by X-Inertia)
+        if ($response instanceof \Illuminate\Http\Response || $response instanceof \Illuminate\Http\JsonResponse) {
+            $response->headers->set('Vary', 'X-Inertia', false);
+        }
+        
+        return $response;
+    }
+
+    /**
      * The root template that's loaded on the first page visit.
      *
      * @see https://inertiajs.com/server-side-setup#root-template
