f742f42babb7ed98a117dd23d2bea41ec999ffb1
EngAya <aya.ghoury@gmail.com>
2026-09-25 20:46:00 +0300

    add BelongsToTenant trait, guarded ID, and boolean cast to EvaluationType model

--- app/Models/EvaluationType.php
+++ app/Models/EvaluationType.php
@@ -6,5 +6,11 @@ use Illuminate\Database\Eloquent\Model;
 
 class EvaluationType extends Model
 {
-    //
+    use \App\Traits\BelongsToTenant;
+
+    protected $guarded = ['id'];
+
+    protected $casts = [
+        'is_active' => 'boolean',
+    ];
 }
